6273da0cbaed5f88d11cb9440cf5c852d1e815d3
Mariano Eret Torres <MJERET>
2024-09-23 22:03:28 +0200

    Ficheros nuevos mas cambio de nombres

---
+++ DWEC/1_Javascript/javascript.js
@@ -0,0 +1,22 @@
+    
+    // En javascript declaramos funciones como en java que vayan a para en nuestro archivo html con referecnia.
+    //En javascript tenemos varias etiquetas para para hacer cosas en nuestras funciones.
+    
+    var nombre;
+    var nombre ="Juann" // Se declara y SE ASIGNA valor.
+    nombre = 'Pepe';
+    var nombre = "Juan", apellido = 'García';
+        apellido2 = "López"
+
+    var nombreApellido = "Juan" + " " + "García";
+    var nombreApellido2 = nombre + " " + apellido2;
+    var nacimiento;
+    var edad = nacimiento + 32;
+    a = "1"; b = 2;
+    var c = a + b;
+    
+    // let Limita el ámbito al bloque de ejecución, expresión o declaración.
+    // var limita el ámbito a la función en la que se define o convierte a la variable de ámbito global si está fuera de una función. Se pueden usar y declarar más adelante a diferencia de las creadas con let.
+
+    //Preparar hasta 
+
---
+++ DWEC/1_Javascript/javascript1.js
@@ -0,0 +1,14 @@
+    
+    // En javascript declaramos funciones como en java que vayan a para en nuestro archivo html con referecnia.
+    //En javascript tenemos varias etiquetas para para hacer cosas en nuestras funciones.
+    
+    
+    function hiMessage() {
+        window.alert(hola);
+        
+    }
+
+    function Mathdosmastres () {
+        window.alert(2+3);
+        document.writer();
+    }
