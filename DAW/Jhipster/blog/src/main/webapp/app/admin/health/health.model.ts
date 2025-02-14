dfdd67b254f5ff68687bdfab0bc03e38ba726308
Mariano Eret Torres <MJERET>
2025-02-14 04:03:06 +0100

    Initial version of blog generated by generator-jhipster@8.9.0

---
+++ DAW/Jhipster/blog/src/main/webapp/app/admin/health/health.model.ts
@@ -0,0 +1,13 @@
+export type HealthStatus = 'UP' | 'DOWN' | 'UNKNOWN' | 'OUT_OF_SERVICE';
+
+export type HealthKey = 'diskSpace' | 'mail' | 'ping' | 'livenessState' | 'readinessState' | 'db';
+
+export interface Health {
+  status: HealthStatus;
+  components?: Partial<Record<HealthKey, HealthDetails>>;
+}
+
+export interface HealthDetails {
+  status: HealthStatus;
+  details?: Record<string, unknown>;
+}
---
+++ DAW/Jhipster/blog/src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'findLanguageFromKey',
+})
+export default class FindLanguageFromKeyPipe implements PipeTransform {
+  private readonly languages: Record<string, { name: string; rtl?: boolean }> = {
+    es: { name: 'Español' },
+    en: { name: 'English' },
+    // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+  };
+
+  transform(lang: string): string {
+    return this.languages[lang].name;
+  }
+}
